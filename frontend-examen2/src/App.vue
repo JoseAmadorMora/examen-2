96ca4bc31be5f69c5cad993abdaa2483ca74c716
Jose <jose.amadormora@ucr.ac.cr>
2024-12-04 20:11:11 -0600

    se crea proyecto de vue

---
+++ frontend-examen2/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <VendingMachine/>
+</template>
+
+<script>
+import VendingMachine from './VendingMachine.vue'
+
+export default {
+  name: 'App',
+  components: {
+    VendingMachine
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
